30bbbb1cffcf82955230664e42f14b545c85e060
Dean <systemsconsciousness@gmail.com>
2023-04-24 20:39:02 -0400

    Testing cookies 3

--- middleware.ts
+++ middleware.ts
@@ -6,9 +6,15 @@ export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get('authorization')
   const url = req.nextUrl
 
+  let cookie = req.cookies.get('nextjs')?.value
+  console.log(cookie) // => 'fast'
+  const allCookies = req.cookies.getAll()
+  console.log(allCookies) // => [{ name: 'nextjs', value: 'fast' }]
+
   if (basicAuth) {
     const authValue = basicAuth.split(' ')[1]
     const [user, pwd] = atob(authValue).split(':')
+    
 
     if (user === process.env.BASIC_AUTH_USERNAME && pwd === process.env.BASIC_AUTH_PASSWORD) {
       const response = NextResponse.next()
