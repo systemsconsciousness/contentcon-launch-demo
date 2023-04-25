c35f87b05c306ded2417e872aa01d03da8ce4fa2
Dean <systemsconsciousness@gmail.com>
2023-04-24 21:12:22 -0400

    Revert tests

--- middleware.ts
+++ middleware.ts
@@ -6,22 +6,12 @@ export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get('authorization')
   const url = req.nextUrl
 
-  let cookie = req.cookies.get('nextjs')?.value
-  console.log(cookie) // => 'fast'
-  const allCookies = req.cookies.getAll()
-  console.log(allCookies) // => [{ name: 'nextjs', value: 'fast' }]
-
   if (basicAuth) {
     const authValue = basicAuth.split(' ')[1]
     const [user, pwd] = atob(authValue).split(':')
-    
-    console.log("Hey Dean!")
+
     if (user === process.env.BASIC_AUTH_USERNAME && pwd === process.env.BASIC_AUTH_PASSWORD) {
-      const response = NextResponse.next()
-      response.cookies.set('vercel', 'fast')
-      cookie = response.cookies.get('vercel')
-      console.log(cookie) // => { name: 'vercel', value: 'fast', Path: '/test' }
-      return response
+      return NextResponse.next()
     }
   }
   url.pathname = '/api/basicauth'
